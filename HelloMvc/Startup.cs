15e6dec47f2a17ba088046e69e2c4e41ebe0b600
Martin Tracey <matracey@outlook.ie>
2016-04-12 17:43:33 +0100

    Added customAddress ServerFeature.

--- HelloMvc/Startup.cs
+++ HelloMvc/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Server.Features;
+
 namespace HelloMvc
 {
     public class Startup
@@ -16,6 +19,12 @@ namespace HelloMvc
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(LogLevel.Debug);
+            
+            // Define custom address.
+            string customAddress = "http://ubuntu:8080/";
+            
+            // Add it to the ICollection<string> of ServerAddresses.
+            app.ServerFeatures.Get<IServerAddressesFeature>()?.Addresses.Add(customAddress);
 
             app.UseStaticFiles();
 
